9b8c7653421bbc8cbacb232ef1ffab37b7477f6f
Juan Araque <kismu@Juans-MacBook-Pro-2.local>
2023-03-19 01:28:41 -0500

    feat: config storybook;

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,3 +1,7 @@
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { createGlobalStyle } from 'styled-components';
+import '@fontsource/roboto';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +11,20 @@ export const parameters = {
     },
   },
 };
+
+const GlobalStyles = createGlobalStyle`
+   *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: Roboto;
+    }
+`;
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles, // Adds your GlobalStyle component to all stories
+  }),
+];
